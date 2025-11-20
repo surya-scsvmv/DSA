fbd056c42e822799f39c3f52c804e161509e3a4d
Penumarthi Phanindra Surya Prabhas <11249a284@kanchiuniv.ac.in>
2025-11-20 19:29:42 +0530

    Update 01. LinearSearch.c

--- 01. LinearSearch.c
+++ 01. LinearSearch.c
@@ -1,30 +1,38 @@
 /*
-Aim:
-To perform Linear Search on an array to find a given element.
+  Aim:
+  Perform Linear Search on an array and locate a given element.
 */
 
 #include <stdio.h>
 
 int main() {
     int n, i, key;
+
+    // Take array size from user
     printf("Enter size of array: ");
     scanf("%d", &n);
 
-    int a[n];
-    printf("Enter elements: ");
-    for(i=0;i<n;i++)
-        scanf("%d", &a[i]);
+    int a[n];  // Create array of given size
 
+    // Read array elements
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Read the element to search for
     printf("Enter element to find: ");
     scanf("%d", &key);
 
-    for(i=0;i<n;i++) {
-        if(a[i] == key) {
-            printf("Element found at position %d\n", i+1);
+    // Linear search logic: scan from left to right
+    for(i = 0; i < n; i++) {
+        if(a[i] == key) {   // Found the element
+            printf("Element found at position %d\n", i + 1);
             return 0;
         }
     }
 
-    printf("Element not found :(\n");
+    // If loop ends, element wasn’t found
+    printf("Element not found\n");
     return 0;
 }

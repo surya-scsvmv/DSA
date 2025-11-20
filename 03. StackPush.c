a2319556e474da30b4db71da7f2bd07cc40bf696
Penumarthi Phanindra Surya Prabhas <11249a284@kanchiuniv.ac.in>
2025-11-20 19:31:47 +0530

    Update 03. StackPush.c

--- 03. StackPush.c
+++ 03. StackPush.c
@@ -1,29 +1,39 @@
 /*
-Aim:
-To implement stack push operation using arrays.
+  Aim:
+  Implement stack push operation using arrays.
 */
 
 #include <stdio.h>
 #define MAX 5
 
-int s[MAX], top = -1;
+int s[MAX];      // Stack array
+int top = -1;    // Top starts at -1 (empty)
 
+// Push function: inserts an element on top of the stack
 void push(int x) {
-    if(top == MAX-1) {
-        printf("Stack full!\n");
+    if(top == MAX - 1) {       // Check overflow
+        printf("Stack full! Cannot push %d\n", x);
         return;
     }
-    s[++top] = x;
+
+    s[++top] = x;              // Increment top, then add value
     printf("Pushed %d\n", x);
 }
 
 int main() {
     int n, val;
+
+    // Ask how many items user wants to push
     printf("How many items to push (max %d): ", MAX);
     scanf("%d", &n);
-    if(n > MAX) n = MAX;
 
-    for(int i=0;i<n;i++) {
+    // If user enters more than MAX, limit it
+    if(n > MAX) {
+        n = MAX;
+    }
+
+    // Read values and push them one by one
+    for(int i = 0; i < n; i++) {
         printf("Enter value: ");
         scanf("%d", &val);
         push(val);

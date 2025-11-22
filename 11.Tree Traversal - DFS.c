28e5c66d9d13ad04b448479d4c0c7c9470755e7d
Penumarthi Phanindra Surya Prabhas <11249a284@kanchiuniv.ac.in>
2025-11-22 16:02:21 +0530

    Update 11.Tree Traversal - DFS.c

--- 11.Tree Traversal - DFS.c
+++ 11.Tree Traversal - DFS.c
@@ -1,6 +1,12 @@
+/*
+  Aim:
+  Implement a Binary Tree and perform DFS traversals:
+  Preorder, Inorder, and Postorder.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
+
 struct node {
     int data;
     struct node *left, *right;
@@ -38,7 +44,6 @@ void postorder(struct node* root) {
 }
 
 int main() {
-    
 
     struct node* root = newNode(1);
     root->left = newNode(2);
@@ -46,14 +51,4 @@ int main() {
     root->left->left = newNode(4);
     root->left->right = newNode(5);
 
-    printf("DFS - Preorder: ");
-    preorder(root);
-
-    printf("\nDFS - Inorder : ");
-    inorder(root);
-
-    printf("\nDFS - Postorder: ");
-    postorder(root);
-
-    return 0;
-}
+    printf(
